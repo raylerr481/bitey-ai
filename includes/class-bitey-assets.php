35d0448cc69ca2878c728d3ed859f4478da044e1
Rayler Ramirez | BiteFixes <121004818+raylerr481@users.noreply.github.com>
2026-08-20 18:02:32 -0300

    Provide WordPress REST nonce to Bitey frontend

--- includes/class-bitey-assets.php
+++ includes/class-bitey-assets.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) { exit; }
 class Bitey_Assets {
     public function __construct() { add_action('wp_enqueue_scripts', array($this, 'load_assets')); }
     public function load_assets() {
-        $version = defined('BITEY_VERSION') ? BITEY_VERSION : '2.3.5';
+        $version = defined('BITEY_VERSION') ? BITEY_VERSION : '2.4.2';
         $style = BITEY_URL . 'assets/css/bitey-style.css';
         $script = BITEY_URL . 'assets/js/bitey.js';
         $style_file = defined('BITEY_PATH') ? BITEY_PATH . 'assets/css/bitey-style.css' : '';
@@ -16,6 +16,7 @@ class Bitey_Assets {
             'ajax_url' => admin_url('admin-ajax.php'),
             'rest_url' => esc_url_raw(rest_url('bitey/v1/chat')),
             'nonce' => wp_create_nonce('bitey_nonce'),
+            'rest_nonce' => wp_create_nonce('wp_rest'),
             'company_id' => absint(get_option('bitey_company_id', 1)) ?: 1,
             'channel' => 'website',
             'backend_url' => untrailingslashit((string) get_option('bitey_backend_url', 'https://bitefixes-backend.onrender.com')),
